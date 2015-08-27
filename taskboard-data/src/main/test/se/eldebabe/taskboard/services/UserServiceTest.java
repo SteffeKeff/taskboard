aa322fb36f7cbb25da1408d0b4201f29c132d1ff
Stefan De Geer <sdgkeff@gmail.com>
2015-08-27 14:44:48 +0200

    Lagt till Junit paket-struktur och i build path

---
+++ taskboard-data/src/main/test/se/eldebabe/taskboard/services/UserServiceTest.java
@@ -0,0 +1,14 @@
+package se.eldebabe.taskboard.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
