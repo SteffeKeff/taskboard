ca8c1310edce69c52e387a48323f3f9057b021a9
Neeten <sandra.eliasson94@gmail.com>
2015-08-27 14:45:10 +0200

    created constructors fol Issue.class and getters

--- taskboard-data/src/main/java/se/eldebabe/taskboard/data/models/Issue.java
+++ taskboard-data/src/main/java/se/eldebabe/taskboard/data/models/Issue.java
@@ -9,4 +9,17 @@ public class Issue extends AbstractEntity {
 	@Column
 	private String description;
 	
+	protected Issue(){}
+	
+	public Issue(String description)
+	{
+		super();
+		this.description = description;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+	
 }
