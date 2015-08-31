553f862a4a5a70617701f58baad3b7514d67834d
R-Baraka <ramibarakayhc3l@gmail.com>
2015-08-31 12:17:37 +0200

    Klar med Team CRUD metoderna

--- taskboard-data/src/main/java/se/eldebabe/taskboard/data/services/TeamService.java
+++ taskboard-data/src/main/java/se/eldebabe/taskboard/data/services/TeamService.java
@@ -1,5 +1,9 @@
 package se.eldebabe.taskboard.data.services;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import se.eldebabe.taskboard.data.models.Team;
@@ -10,8 +14,25 @@ public class TeamService {
 	@Autowired
 	private TeamRepository teamRepository;
 	
+	@Transactional
 	public Team saveTeam(Team team){
 		return teamRepository.save(team);
+	}
+	
+	@Transactional
+	public Team deleteTeam(Team team){
+		teamRepository.delete(team.getId());
+		return team;
+	}
+	
+	@Transactional
+	public Team updateTeam(Team team){
+		return teamRepository.save(team);
+	}
+	
+	@Transactional
+	public List<Team> findAllTeams(){
+		return (List<Team>) teamRepository.findAll();
 	}
 	
 }
