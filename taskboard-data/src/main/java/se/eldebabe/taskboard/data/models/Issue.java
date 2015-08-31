901a64362ee7cf367595cf0dadd64e9842eba7cf
Neeten <sandra.eliasson94@gmail.com>
2015-08-31 12:09:44 +0200

    formaterat entiteter

--- taskboard-data/src/main/java/se/eldebabe/taskboard/data/models/Issue.java
+++ taskboard-data/src/main/java/se/eldebabe/taskboard/data/models/Issue.java
@@ -4,24 +4,21 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "issues")
-public class Issue extends AbstractEntity {
+public class Issue extends AbstractEntity{
 
 	@Column
 	private String description;
-	
+
 	protected Issue(){}
-	
-	public Issue(String description)
-	{
+
+	public Issue(String description){
 		this.description = description;
 	}
 
-	public String getDescription()
-	{
+	public String getDescription(){
 		return description;
 	}
-	
+
 }
