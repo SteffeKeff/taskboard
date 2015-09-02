5c172dd5954f081c73dfe33405bf45649905c77e
Neeten <sandra.eliasson94@gmail.com>
2015-09-02 11:23:14 +0200

    skapat get metod för web issue

--- taskboard-web/src/main/java/se/eldebabe/services/IssueWebService.java
+++ taskboard-web/src/main/java/se/eldebabe/services/IssueWebService.java
@@ -1,10 +1,32 @@
 package se.eldebabe.services;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+
+import se.eldebabe.taskboard.data.models.Issue;
+import se.eldebabe.taskboard.data.services.IssueService;
 
 @Path("issues")
-public final class IssueWebService {
+public final class IssueWebService{
 
-	
-	
+	private IssueService issueService = new IssueService();
+
+	@Context
+	public UriInfo uriInfo;
+
+	@GET
+	@Path("{issueId}")
+	public final Response getIssue(@PathParam("issueId") final Long id){
+		Issue issue = issueService.findIssueById(id);
+		if(issue == null) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		return Response.ok(issue).build();
+	}
+
 }
