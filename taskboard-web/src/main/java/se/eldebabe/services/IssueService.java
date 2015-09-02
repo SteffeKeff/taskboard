8a87e6e7af425c951a361aece66d496ea0209b12
R-Baraka <ramibarakayhc3l@gmail.com>
2015-09-02 10:36:13 +0200

    Lagt till några serviceklasser

---
+++ taskboard-web/src/main/java/se/eldebabe/services/IssueService.java
@@ -0,0 +1,8 @@
+package se.eldebabe.services;
+
+import javax.ws.rs.Path;
+
+@Path("issues")
+public class IssueService {
+
+}
---
+++ taskboard-web/src/main/java/se/eldebabe/services/WorkItemService.java
@@ -0,0 +1,8 @@
+package se.eldebabe.services;
+
+import javax.ws.rs.Path;
+
+@Path("workItems")
+public class WorkItemService {
+
+}
